1ce77206eed5beeebf7a966f44311306e7bbac5b
Jony <jony.fes90@gmail.com>
2016-06-17 10:50:17 -0300

    1

--- src/Curso.java
+++ src/Curso.java
@@ -1,23 +1,5 @@
 
 
 public class Curso {
-	chau
-	adiosss
-	Esto no anda nada
-	jejlagjkeajgég
-	ajg
-	agj
-	ajga
-	gja
-	gja
-	gag
-	aa
-	
-	LUCHI
-	
-	agljda
-	gadakg
-	akglagk
-	adgka
-	g'lakgád
+	public class whereisSofi {};
 }

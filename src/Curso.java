47c2638cd5ae119bc335119f66c6e2b5610c3eac
Valacco <jsvrojo@gmail.com>
2016-06-17 11:00:23 -0300

    finaltest

--- src/Curso.java
+++ src/Curso.java
@@ -4,11 +4,6 @@
 
 
 public class Curso {
-<<<<<<< HEAD
+
 	private String request = "Please work"
-=======
-	public class whereisSofi {
-		guiye la ms capa
-	};
->>>>>>> branch 'master' of https://github.com/valack/UNICEN-ING-2016.git
 }

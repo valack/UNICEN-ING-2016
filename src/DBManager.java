97b3e5e6b70e6f0c231fbbb98b413579b8de37d8
gbardel <Bardel@Bardel-PC>
2016-06-21 18:16:08 -0300

    Metodos de la clase DBManager

---
+++ src/DBManager.java
@@ -0,0 +1,40 @@
+import java.util.Vector;
+
+
+//Todo las sentencias SQL se van guardando en changes
+//Luego del guardar se ejecuta changes en el postgresql
+public class DBManager {
+	Vector<String>changes;
+	
+	public DBManager(){
+		changes=new Vector<>();
+	}
+	
+	public void createVertex(){
+		//sentencias jdbc para actualizar esto
+	}
+	
+	public void deleteVertex(){
+		//sentencias jdbc para actualizar esto
+	}
+	
+	public void changeVertex(){
+		//sentencias jdbc para actualizar esto
+	}
+	
+	public void createEdge(){
+		//sentencias jdbc para actualizar esto
+	}
+	
+	public void deleteEdge(){
+		//sentencias jdbc para actualizar esto
+	}
+	
+	public void changeEdge(){
+		//sentencias jdbc para actualizar esto
+	}
+	
+	public void importGrafo(){
+		//sentencias jdbc para esto, usar graphviz
+	}
+}

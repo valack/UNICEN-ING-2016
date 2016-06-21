b4925db0a7674964d8ab67e5e5a6fc252e1ed820
Valacco <jsvrojo@gmail.com>
2016-06-21 18:46:25 -0300

    Interfaz del grafo

---
+++ src/Graph.java
@@ -0,0 +1,142 @@
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.jgrapht.*;
+
+public class Graph implements org.jgrapht.Graph<Vertex, Edge>{
+
+	@Override
+	public Set<Edge> getAllEdges(Vertex sourceVertex, Vertex targetVertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Edge getEdge(Vertex sourceVertex, Vertex targetVertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EdgeFactory<Vertex, Edge> getEdgeFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Edge addEdge(Vertex sourceVertex, Vertex targetVertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addEdge(Vertex sourceVertex, Vertex targetVertex, Edge e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addVertex(Vertex v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsEdge(Vertex sourceVertex, Vertex targetVertex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsEdge(Edge e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVertex(Vertex v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Edge> edgeSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Edge> edgesOf(Vertex vertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeAllEdges(Collection<? extends Edge> edges) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Edge> removeAllEdges(Vertex sourceVertex, Vertex targetVertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeAllVertices(Collection<? extends Vertex> vertices) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Edge removeEdge(Vertex sourceVertex, Vertex targetVertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeEdge(Edge e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeVertex(Vertex v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Vertex> vertexSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex getEdgeSource(Edge e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex getEdgeTarget(Edge e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getEdgeWeight(Edge e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	/**
+	 *	
+	 *
+	 *	@author Team 2.1 
+	 */
+	
+	
+}
